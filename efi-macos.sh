61de8dc81703d8a8fc87a44747b385b4083ae3d7
cxnzie <gyattbook77@gmail.com>
2025-01-17 20:21:39 -0500

    fix

--- efi-macos.sh
+++ efi-macos.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 set -e
 
-printf "\nEFI Unlocker 1.0.0 for VMware Fusion\n"
+printf "\nEFI Unlocker 2.0.0 for VMware Fusion\n"
 printf "======================================\n"
 printf "(c) Dave Parsons 2018\n\n"
+printf "(c) conzurstuff.fyi 2024\n\n"
 
 # Ensure we only use unmodified commands
 export PATH=/bin:/sbin:/usr/bin:/usr/sbin
@@ -19,18 +20,19 @@ printf "VMware product version: ${version}.${build}\n\n"
 #printf "Build:    ${build}\n"
 
 # Check version is 10+
-if [[ ${product[0]} -lt 10 ]]; then
-   printf "VMware Fusion version 10 or greater required!\n"
+if [[ ${product[0]} -lt 13 ]]; then
+   printf "VMware Fusion version 13 or greater required!\n"
+   printf "Use the older efi-unlocker for older fusion versions."
    exit 1
 fi
 
 printf "Patching 32-bit ROM...\n"
-cp -v /Applications/VMware\ Fusion.app/Contents/Library/roms/EFI32.ROM .
+cp -v /Applications/VMware\ Fusion.app/Contents/Library/roms/x86_x64/EFI32.ROM .
 ./macos/UEFIPatch EFI32.ROM patches.txt -o EFI32-MACOS.ROM
 rm -fv EFI32.ROM
 
 printf "\nPatching 64-bit ROM...\n"
-cp -v /Applications/VMware\ Fusion.app/Contents/Library/roms//EFI64.ROM .
+cp -v /Applications/VMware\ Fusion.app/Contents/Library/roms/x86_x84/EFI64.ROM .
 ./macos/UEFIPatch EFI64.ROM patches.txt -o EFI64-MACOS.ROM
 rm -fv EFI64.ROM
 
